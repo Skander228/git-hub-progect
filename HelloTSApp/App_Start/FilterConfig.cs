035d4816d5b6f47e2f09fa1bafd225db693982a2
kkhemiri_skander <kkhemiriskander@gmail.com>
2019-10-24 17:15:08 +0300

    Добавление репозитория

---
+++ HelloTSApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HelloTSApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
